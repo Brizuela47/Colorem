c08575dd57f05cdc7c37180d64b1f24780773960
Fede <fedelva8@gmail.com>
2021-11-19 17:09:05 -0300

    diseño login y register

---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div>
+        <a class="volver-btn" href="index.php">Volver</a>
+    </div>
+    <input class="input-nombre" type="text" placeholder="Nombre">
+    <br>
+    <input class="input-nombre" type="password" placeholder="Contra">
+    <br>
+    <input class="input-ingresar" type="submit" value="Registrar">
+    <div>
+        <small>
+            Ya tenes una cuenta? <a class="volver-btn2" href="login.php">Ingresa</a>
+        </small>
+    </div>
+</body>
+</html>
